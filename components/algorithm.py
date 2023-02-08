97d7e7cc30388160f18c35bdf8aee0dc5ee48c52
amirhossein mobini <amirhosseinmobini9@gmail.com>
2023-02-08 17:17:20 +0330

    alphabet added to project

--- components/algorithm.py
+++ components/algorithm.py
@@ -1,11 +1,33 @@
-number = int(input(": "))
-radix = int(input(": "))
+def number_to_alphabet(number):
+    if number < 10:
+        return number
+    elif number == 10:
+        return "A"
+    elif number == 11:
+        return "B"
+    elif number == 12:
+        return "C"
+    elif number == 13:
+        return "D"
+    elif number == 14:
+        return "E"
+    elif number == 15:
+        return "F"
+    elif number == 16:
+        return "G"
+    elif number == 17:
+        return "H"
+    elif number == 18:
+        return "I"
+    elif number == 19:
+        return "J"
 
 
 def encrypt(number, radix):
     changed_number = []
     while number > (mod := 0):
         mod = number % radix
+        mod = number_to_alphabet(mod)
         number = number // radix
         changed_number.append(mod)
     return changed_number.reverse()
