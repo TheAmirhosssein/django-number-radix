e565c9788668ff7743453d4a14205045d7faeee3
amirhossein mobini <amirhosseinmobini9@gmail.com>
2023-02-13 18:29:35 +0330

    encrypt url created

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from main.views import DecryptView, HomePageView
+from main.views import DecryptView, EncryptView, HomePageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home-page"),
     path("decrypt/", DecryptView.as_view(), name="decrypt-page"),
+    path("encrypt/", EncryptView.as_view(), name="encrypt-page"),
 ]
