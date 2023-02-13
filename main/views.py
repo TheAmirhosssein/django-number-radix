298518d3ee13b40e7e8071fc48a738422342092a
amirhossein mobini <amirhosseinmobini9@gmail.com>
2023-02-13 21:40:28 +0330

    encrypt operation view created

--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
+
+from components.algorithm import encrypt
 
 
 class HomePageView(TemplateView):
@@ -12,3 +15,16 @@ class DecryptView(TemplateView):
 
 class EncryptView(TemplateView):
     template_name = "main/encrypt.html"
+
+
+def encrypt_operation(request: HttpRequest):
+    number = int(request.GET.get("number"))
+    radix = int(request.GET.get("radix"))
+    final_number = encrypt(number, radix)
+    if radix > 20:
+        final_number = "your radix must be lower than 21"
+    return render(
+        request,
+        template_name="main/includes/encrypt_result.html",
+        context={"result": final_number},
+    )
