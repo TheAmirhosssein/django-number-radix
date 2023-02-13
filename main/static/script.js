61234caf278d52bfc0643bd62293e98f2811e449
amirhossein mobini <amirhosseinmobini9@gmail.com>
2023-02-13 21:52:20 +0330

    decrypt operation js function created

--- main/static/script.js
+++ main/static/script.js
@@ -8,3 +8,15 @@ function encrypt() {
         $('#resultArea').html(res);
     });
 }
+
+
+function decrypt() {
+    var number = $('#numberInput').val();
+    var radix = $('#radixInput').val();
+    $.get('/decrypt/operation/', {
+        number: number,
+        radix: radix,
+    }).then(res => {
+        $('#resultArea').html(res);
+    });
+}
