afe6062c543f2ba79c33db6ff33d7126b7b46b3e
amirhossein mobini <amirhosseinmobini9@gmail.com>
2023-02-13 21:53:20 +0330

    decrypt operation url created

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
-from main.views import DecryptView, EncryptView, HomePageView, encrypt_operation
+from main.views import (
+    DecryptView,
+    EncryptView,
+    HomePageView,
+    decrypt_operation,
+    encrypt_operation,
+)
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home-page"),
     path("decrypt/", DecryptView.as_view(), name="decrypt-page"),
+    path("decrypt/operation/", decrypt_operation, name="decrypt-operation"),
     path("encrypt/", EncryptView.as_view(), name="encrypt-page"),
     path("encrypt/operation/", encrypt_operation, name="encrypt-operation"),
 ]
